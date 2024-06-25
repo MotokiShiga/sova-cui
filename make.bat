c7819175a4f4a156eab7f9739c095485b6c04607
H.Morita <penelope.hide.morita@gmail.com>
2024-06-25 11:17:47 +0900

    Windows版のインストーラ作成

--- make.bat
+++ make.bat
@@ -1,3 +1,19 @@
+rem histogram.pyd
 cd sova\computation\histogram
-
 python setup.py build_ext -i
+rmdir /s /q build
+
+rem calculate_domain_centers.pyd
+cd ..\split_and_merge\domain_centers
+python setup.py build_ext -i
+rmdir /s /q  build
+
+rem calculate_domain_centers.pyd
+cd ..\util\numpy_extension
+python setup.py build_ext -i
+rmdir /s /q  build
+
+rem cif2cell
+cd ..\..\..\..\libs\cif2cell
+python setup.py build_ext -i
+rmdir /s /q  build
