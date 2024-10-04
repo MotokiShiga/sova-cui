db000883a6c530313d5a3607e65becfe0cdfb080
Motoki Shiga <motoki.shiga.b4@tohoku.ac.jp>
2024-10-04 12:03:38 +0900

    Change the example of cavity analisis to display cavity properties.

--- example/python_script/8_cavity.py
+++ example/python_script/8_cavity.py
@@ -16,17 +16,31 @@ atoms = f.getatoms()
 cavity = Cavity(atoms)
 
 # Calculate cavities
-# use default radii
-cavity.calculate(resolution=64)
+# # use default radii
+# cavity.calculate(resolution=64)
 
-# set same radii for all elements
-# cavity.calculate(resolution=64, cutoff_radii=2.8, gyration_tensor_parameters=False)
+# Set same radii for all elements
+# if gyration_tensor_parameters = True, cavity shape parameters are calculated.
+cavity.calculate(resolution=64, cutoff_radii=2.8, gyration_tensor_parameters=True)
 
-# set custom radii
-# cavity.calculate(resolution=64, cutoff_radii={'Si': 2.0, 'O': 3.0}, gyration_tensor_parameters=False)
+# Set custom radii
+# cavity.calculate(resolution=64, cutoff_radii={'Si': 2.0, 'O': 3.0}, gyration_tensor_parameters=True)
 
-### Display caluculation results
-print("Calculate domains")
+
+### Caluculation results
+#Three types of cavities: domain, center-based cavity, and surface-based cavity, are calculated. 
+#Each of calculated results is obtained by "cavity.domain", "cavity.center_cavities", or "cavity.surface_cavities".
+print("The number of domain:")
+print(cavity.domains.number)
+
+print("\nThe number of center-based cavities:")
+print(cavity.center_cavities.number)
+
+print("\nThe number of surface-based cavities:")
+print(cavity.surface_cavities.number)
+
+# Display some properties
+print("\n\nCalculate domains")
 print('Found {:d} domains'.format(len(cavity.domains.volumes)))
 index =  0
 print('Domain volume of index {} : {}'.format(index, cavity.domains.volumes[index]))
@@ -34,8 +48,26 @@ print('Found {:d} critical domains in file {}. Domain indices: {}'.format(
     len(cavity.domains.critical_domains), os.path.basename(structure_file), 
     cavity.domains.critical_domains))
 
-plt.figure(figsize=(6,3))
-plt.hist(cavity.domains.volumes)
-plt.xlabel('Volume')
-plt.ylabel('Counts')
+
+### Histogram of cavity properties
+#
+# For the definitions of cavity properties, refer to the "pyMolDyn" paper:  
+#  I. Heimbach, F. Rhiem, F. Beule, D. Knodt, J. Heinen, and R.O. Jones.,  
+#  "pyMolDyn: Identification, structure, and properties of cavities/vacancies in condensed matter and molecules,"  
+#  J. Comput. Chem., 38, 389–394 (2017).   
+#   https://onlinelibrary.wiley.com/doi/full/10.1002/jcc.24697
+
+attrs = ['volumes', 'surface_areas', 'squared_gyration_radii', 
+         'asphericities', 'acylindricities', 'anisotropies']
+
+plt.figure(figsize=(12,5))
+for cnt, attr in enumerate(attrs):
+    plt.subplot(2,3,cnt+1)
+    vals = getattr(cavity.domains, attr)
+    # vals = getattr(cavity.center_cavities, attr)
+    # vals = getattr(cavity.surface_cavities, attr)
+    plt.hist(vals)
+    plt.xlabel(attr)
+    plt.ylabel('Counts')
+plt.tight_layout()
 plt.show()
